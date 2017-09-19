b8351dcf65bf9c87a614513c23ce9559de517680
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2017-09-19 11:03:56 +0200

    implement to encrypt using master key

--- client.py
+++ client.py
@@ -2,7 +2,6 @@ from diffiehellman.diffiehellman import DiffieHellman
 import socket, string, random, hashlib
 from cypher import AESCipher 
 
-
 # Generate keypair
 alice = DiffieHellman()
 alice.generate_public_key()    # automatically generates private key
@@ -21,6 +20,33 @@ session = ''
 def random_string(N):
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
 
+def send_message(msg, session, sequence_number):
+    # Create msg of msg,session and sequence
+    complete_msg = ':::::'.join([msg, str(session), str(sequence_number)])
+
+    # Create hash of msg
+    hashvalue = hashlib.sha256(complete_msg.encode('utf-8')).digest()
+    
+    # append hash
+    complete_msg = '%%%%%%'.join(['START', complete_msg, str(hashvalue), 'END'])
+
+    # Encrypt
+    encrypted = aesciph.encrypt(complete_msg)
+
+    # Send to server
+    sock.sendto(encrypted, address)
+
+    # Increment sequence
+    return sequence_number + 1
+
+def recieve_response():
+    echo, serv_addr = sock.recvfrom(4096)
+    return echo.decode('utf-8')
+
+
+# ===========================
+# Here starts the handshake
+
 #def init_handshake():
 handshake = "PUBLIC_KEY," + str(alice.public_key)
 msg = handshake.encode('utf-8')
@@ -30,51 +56,31 @@ bob_public_key, serv_addr = sock.recvfrom(4096)
 bob_public_key = int(bob_public_key.decode('utf-8'))
 
 alice.generate_shared_secret(bob_public_key, echo_return_key=True)
+aesciph = AESCipher(alice.shared_key)
 
 print("sharedkey;", alice.shared_key)
 print("=======================")
 
-session = random_string(32)
-sessionmsg = "SESSION," + session 
+session = random.getrandbits(128)
+sessionmsg = "SESSION_SET" 
 print("session:", sessionmsg)
 print("=======================")
-sock.sendto(sessionmsg.encode('utf-8'), address)
-
+#sock.sendto(sessionmsg.encode('utf-8'), address)
+send_message(sessionmsg, session, 0)
 
 #init_handshake()
 
-sequence_number = 0
-def send_message(msg, session, sequence_number):
-    # Increment sequence
-    #sequence_number += 1  
-    sequence_number = 1  
+sequence_number = 1
 
-    # Create msg of msg,session and sequence
-    complete_msg = ':::::'.join([msg, session, str(sequence_number)])
+# ===========================
+# Here starts the communication
 
-    # Create hash of msg
-    hashvalue = hashlib.sha256(complete_msg.encode('utf-8')).digest()
-    
-    # append hash
-    complete_msg = '%%%%%%'.join([complete_msg, str(hashvalue)])
+# Use session as masterkey to encrypt instead
+aesciph = AESCipher(str(session))
 
-    # Encrypt
-    encrypted = aesciph.encrypt(complete_msg)
-
-    # Send to server
-    sock.sendto(encrypted, address)
-
-def recieve_response():
-    echo, serv_addr = sock.recvfrom(4096)
-    return echo.decode('utf-8')
-
-
-
-
-aesciph = AESCipher(alice.shared_key)
 while True:
     message = input("Enter a message to send: ")
 
-    send_message(message, session, sequence_number)
+    sequence_number = send_message(message, session, sequence_number)
     resp = recieve_response()
     print(resp)
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from diffiehellman.diffiehellman import DiffieHellman
-import socket, hashlib
+import socket, hashlib, random, string
 from cypher import AESCipher 
+
+def random_string(N):
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
 
 # Generate keypair
 bob = DiffieHellman()
@@ -35,51 +38,58 @@ while True:
 
         sock.sendto(bobkey, client_addr)
         
+        sessiontable["sharedkey" + random_string(32)] = bob.shared_key
+
         print("shared key:", bob.shared_key)
         print("=======================")
 
-    elif(msg.startswith("SESSION")):
-        session = msg.split(',')[1]
-        print("session:", session)
-        print("=======================")
-
-    elif session != '':
-
-        # Decyphir
-        aesciph = AESCipher(bob.shared_key)
-        decrypted = aesciph.decrypt(data)
-
-        # Extract hash
-        complete_msg, hashvalue = decrypted.strip().split('%%%%%%')
-
-        # Verify hash
-        newhash = hashlib.sha256(complete_msg.encode('utf-8')).digest()
-
-        aoeu = ('%s' % newhash)
-
-        if aoeu != hashvalue:
-            print(aoeu)
-            print(type(aoeu))
-            print(hashvalue)
-            print(type(hashvalue))
-            print(hashvalue == newhash)
-            sock.sendto("Hash unverified".encode('utf-8'), client_addr)
-            continue
-
-        # Extract message
-        message, session, sequence_number = complete_msg.split(':::::')
-
-        print('Seq: ', sequence_number)
-
-        # Respond
-        sock.sendto(message.encode('utf-8'), client_addr)
-        print(decrypted)
-
-
-
-
-
     else:
-        sock.sendto("No shared key exists".encode('utf-8'), client_addr)
-        print("No shared key exists")
+        has_shared_key = False
+        
+        keyvalues = sessiontable.values()
+        for masterkey in list(keyvalues):
+            # Decyphir
+            aesciph = AESCipher(masterkey)
+            try:
+                decrypted = aesciph.decrypt(data)
+            except:
+                continue
+
+            # Extract hash
+            decrypted = decrypted.strip().split('%%%%%%')
+            if len(decrypted) != 4:
+                continue
+            start, complete_msg, hashvalue, end = decrypted
+
+            # Verify start and end flags
+            if start != 'START' or end != 'END':
+                continue
+
+            # Verify hash
+            newhash = hashlib.sha256(complete_msg.encode('utf-8')).digest()
+
+            # Convert newhash to string and check against hashvalue
+            if ('%s' % newhash) != hashvalue:
+                sock.sendto("Hash unverified".encode('utf-8'), client_addr)
+                continue
+
+            # Extract message
+            message, session, sequence_number = complete_msg.split(':::::')
+
+            if(message == "SESSION_SET" and sequence_number == '0'):
+                has_shared_key = True
+                sessiontable[masterkey] = session;
+                print("session:", session)
+                print("=======================")
+                continue;
+
+            # Respond
+            has_shared_key = True
+            sock.sendto(message.encode('utf-8'), client_addr)
+            print(decrypted)
+
+        if not has_shared_key:
+            sock.sendto('==No shared key found'.encode('utf-8'), client_addr)
+            
+            
 
